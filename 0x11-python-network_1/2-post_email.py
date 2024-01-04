693d2d5969e1eca03b8a7abb89bf99e8978c7f91
angolosFlame <couragetkordzi@gmail.com>
2024-01-04 10:41:01 -0800

    0x14. JavaScript - Web scraping

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,18 +1,20 @@
 #!/usr/bin/python3
+# -*- coding: utf-8 -*-
 """This scrpt Sends a POST request to a given URL with a given email.
 Usage: ./2-post_email.py <URL> <email>
   - Displays the body of the response.
 """
-import sys
-import urllib.parse
-import urllib.request
+from urllib.request import urlopen, Request
+from urllib.parse import urlencode
+from sys import argv
 
 
-if __name__ == "__main__":
-    url = sys.argv[1]
-    value = {"email": sys.argv[2]}
-    data = urllib.parse.urlencode(value).encode("ascii")
-
-    request = urllib.request.Request(url, data)
-    with urllib.request.urlopen(request) as response:
-        print(response.read().decode("utf-8"))
+if __name__ == '__main__':
+    url = argv[1]
+    email = {'email': argv[2]}
+    email = urlencode(email)
+    email = email.encode('ascii')
+    request = Request(url, email)
+    with urlopen(request) as response:
+        string = response.read().decode('utf-8')
+        print(string)
